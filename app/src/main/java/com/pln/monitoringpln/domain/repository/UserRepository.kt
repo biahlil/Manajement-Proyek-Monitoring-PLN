5a5469fc90274164f18bb677b87c7a743b298b44
biahlil <2010817110011@msh.ulm.ac.id>
2025-11-20 00:18:00 +0800

    Reapply "feat: Implement user active status check on login"
    This reverts commit 8ec0b74ea71be0b510121add3311c5735301744a.

--- app/src/main/java/com/pln/monitoringpln/domain/repository/UserRepository.kt
+++ app/src/main/java/com/pln/monitoringpln/domain/repository/UserRepository.kt
@@ -5,4 +5,7 @@ import com.pln.monitoringpln.domain.model.User
 interface UserRepository {
     // Kita butuh 'suspend' karena ini operasi jaringan/async
     suspend fun login(email: String, password: String): Result<User>
+    suspend fun addTeknisi(email: String, password: String, namaLengkap: String): Result<User>
+    suspend fun getTeknisiDetail(id: String): Result<User>
+    suspend fun setUserStatus(id: String, isActive: Boolean): Result<Unit>
 }
